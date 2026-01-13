8cebce2fce611e57bb0000dc5c27ad43d00942df
Farook Ologundudu <ologundudufob@gmail.com>
2026-01-14 00:07:52 +0100

    Day 8: More Exercises Just uploading the first task for now

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -253,34 +253,65 @@ import 'dart:io';
 // Bonus Challenge: Create a second Set called vipGuests with the names ['Alice', 'David']. Use a Set
 //method to find out which guests in your finalGuestList are also on the vipGuests list.
 
-void main() {
-  List<String> rawGuestList = [
-    'Alice',
-    'Bob',
-    'Charlie',
-    'Alice',
-    'David',
-    'Bob',
-    'Eve',
-  ];
+// void main() {
+//   List<String> rawGuestList = [
+//     'Alice',
+//     'Bob',
+//     'Charlie',
+//     'Alice',
+//     'David',
+//     'Bob',
+//     'Eve',
+//   ];
 
-  Set<String> finalGuestList = {...rawGuestList};
+//   Set<String> finalGuestList = {...rawGuestList};
 
-  finalGuestList.add('Frank');
-  finalGuestList.remove('Charlie');
+//   finalGuestList.add('Frank');
+//   finalGuestList.remove('Charlie');
 
-  if (finalGuestList.contains('Alice')) {
-    print('Alice is invited: true');
-  }
+//   if (finalGuestList.contains('Alice')) {
+//     print('Alice is invited: true');
+//   }
 
-  print(finalGuestList);
-  print('The total number of guests is: ${finalGuestList.length}');
+//   print(finalGuestList);
+//   print('The total number of guests is: ${finalGuestList.length}');
 
-  Set<String> vipGuests = {'Alice', 'David', 'Andrew'};
+//   Set<String> vipGuests = {'Alice', 'David', 'Andrew'};
 
-  print(vipGuests.intersection(finalGuestList));
-}
+//   print(vipGuests.intersection(finalGuestList));
+// }
 
-// Day 6 continued: Exercise on Collections so far(Maps, Lists, spread operator)
-// Exercise 3
-// The Task:
+// Day 8: Exercise on Collections so far(Maps, Lists and Sets)
+// Exercise 4
+// The Task: The "Library Sanitizer"
+// You are merging two shipments of books into the library’s master database.
+
+// 1. The Raw Data (The Mess)
+// The Database (Map): Map<String, String> catalog where Key is Title and Value is Genre.
+
+// {'1984': 'Dystopian', 'The Hobbit': 'Fantasy'}
+
+// Shipment A (List): ['1984', 'The Great Gatsby', 'Brave New World']
+
+// Shipment B (List): ['The Hobbit', 'Brave New World', 'The Shining']
+
+// The Blacklist (Set): {'The Shining'} (This book is banned; it must never enter the inventory).
+
+// 2. The Requirements (The Logic)
+// Merge & Deduplicate: Use the Spread Operator to combine Shipment A and Shipment B into one list.
+//Then, move them into a Set to automatically kill any duplicates (like 'Brave New World').
+
+// Filter: Remove any book that exists in the Blacklist.
+
+// Audit: Create a final List of strings called finalReport.
+//For every book that survived the cleaning process:
+
+// Look up its genre in the catalog.
+
+// If it’s in the catalog, format the string as: "Title: [Name] | Genre: [Genre]"
+
+// If it’s not in the catalog (Null), format it as: "Title: [Name] | Genre: Unclassified"
+
+// The Summary: At the very end, print: "Total unique, legal books: [Count]"
+
+void main() {}
